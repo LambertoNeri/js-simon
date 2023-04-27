4ec4854fe537bb2c4df0e6da985d9a55d830d4f6
LambertoNeri <neri.lamberto91@gmail.com>
2023-04-27 22:16:19 +0200

    add 2 functions to have milliseconds from today to tomorrow and transform them into seconds

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-const now = new Date('2023-04-28T09:30:30').getTime()
-console.log (now)
+let now = new Date().getTime()
+let future = new Date('2023-04-28T09:30:30').getTime()
+console.log ('data di oggi',now)
+console.log ('data di domani',future)
+let daMillisecondare 
+let secondsToTomorrow
+
+console.log ('millisecondi mancanti a domani', getDateDifference(now, future))
+console.log ('this is transform', transformToSeconds(daMillisecondare))
+
+
+
+
+function getDateDifference (data1, data2) {
+    daMillisecondare = (data2 - data1)
+    return (data2 - data1)
+}
+    
+function transformToSeconds (date) {
+    secondsToTomorrow = date / 1000
+    return date / 1000
+ }
